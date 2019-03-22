65cacee552bfd293674999f3fb96d485a7f74580
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-03-23 00:48:42 +0100

    initial commit

---
+++ config/utils.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	port: process.env.PORT,
+	db: 'mongodb://localhost/loginapp',
+	secret: 'skb7474h8c7e6b988bb9kpc86307b'
+
+}
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express'),
+			bodyParser = require('body-parser'),
+			mongoose = require('mongoose'),
+			dotenv = require('dotenv'),
+			cors = require('cors'),
+			passport = require('passport'),		
+			jwt = require('jsonwebtoken')
+
+let config = require('./config/utils')
+let User = require('./model/user')
+const authenticate = require('./config/passport')
+
+dotenv.config()
+
+const PORT = config.port || 5500
+
+//initialize server
+let app = express()
+
+//database connection
+mongoose.connect(	config.db,
+	{useNewUrlParser: true}, (err, db)=>{
+ if(err){
+ 	console.log(err)
+ }else{
+ 	console.log('database connected')
+ }
+})
+
+app.use(cors() )
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+
+app.use(passport.initialize() )
+
+
+
+app.post('/register', (req,res)=>{
+
+	User.findOne({email: req.body.email}, (err,user)=>{
+		if(err){
+			throw err
+		}
+		if(user) {
+			res.json({message: 'user already exists'})
+		}else{
+			User.register(new User({
+				name: req.body.name,
+				username: req.body.username,
+				email: req.body.email
+			}), req.body.password, (err,user)=>{
+				if(err) {
+					throw err
+				}else{
+					passport.authenticate('local')(req,res,()=>{
+						return res.json({
+							message: "User registered"
+						})
+					})
+				}
+			})
+		}
+	})
+
+})
+
+app.post("/login", (req,res)=>{
+	passport.authenticate('local')(req,res, ()=>{
+		console.log(req.user._id)
+		const payload = {
+			id: req.user._id,
+			name: req.user.username
+		}
+		jwt.sign(payload, config.secret, (err,token) =>{
+			if(err) {
+				throw err
+			}else{
+				return res.json({
+					message: "Login Success",
+					token
+				})
+			}
+		})
+	})
+})
+
+app.get("/secure",authenticate.verifyUser, (req,res)=>{
+	return res.json({
+		message: 'User verified'
+	})
+})
+
+app.listen(PORT, ()=>{
+	console.log(`server booted @ ${PORT}`)
+})
